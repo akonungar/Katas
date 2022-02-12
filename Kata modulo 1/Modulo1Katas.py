6f152b6be01643bff8f82739579fdca098a2e625
Angel Abel Reyes Aguillon <99379275+akonungar@users.noreply.github.com>
2022-02-11 20:59:58 -0600

    actualizacion de archivos locales

--- Kata modulo 1/Modulo1Katas.py
+++
@@ -1,4 +0,0 @@
-print("Oficial superior, por favor indique el numero de parsec que desea convertir en lightyears:")
-parsec=input()
-lightyears= 3.16156 * int(parsec)
-print(str(parsec)+" parsec son " + str(lightyears)+" lightyears")
